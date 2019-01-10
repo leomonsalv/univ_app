4f8fa8c2161e3ef44264fb46816f85f1c972b70c
Leonardo Monsalve <leonardomonsalv@gmail.com>
2019-01-10 15:49:32 +0000

    Add secure password and update forms

--- Gemfile
+++ Gemfile
@@ -25,10 +25,11 @@ gem 'jbuilder', '~> 2.5'
 gem 'materialize-sass', '~> 1.0.0' 
 gem 'material_icons' 
 gem 'jquery-rails'
+gem 'bcrypt', '~> 3.1.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_002150) do
+ActiveRecord::Schema.define(version: 2019_01_10_143628) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_01_04_002150) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
